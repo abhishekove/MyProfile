c7ee5d22104435a0e9137c1d97df693030ee99d2
abhishekove <abhishekove@gmail.com>
2020-04-28 16:42:50 +0530

    first commit

---
+++ precache-manifest.2100c5da4295a15c8953532a6fa604a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9d846aea0293740e76b3d1da082fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a5781077c2a857fe3a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "503ec598d45b935d1022",
+    "url": "/static/js/2.8da84556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8da84556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a5781077c2a857fe3a",
+    "url": "/static/js/main.28ab5a86.chunk.js"
+  },
+  {
+    "revision": "ca5d3f88fe15cd95685f",
+    "url": "/static/js/runtime-main.260858d2.js"
+  }
+]);
